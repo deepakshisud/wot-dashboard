a099e18444b29331c1bef647ad669c7ef31a523b
Deepakshi <deepakshisud@gmail.com>
2021-03-18 23:46:40 +0530

    Database setup done

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const methodOverride = require('method-override');
+const User = require('./models/user');
 
-app.get('/home', (req, res) => {
+mongoose.connect('mongodb://localhost:27017/wot-dashboard', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, '/views'));
+app.engine('ejs',ejsMate);
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+const sessionConfig = {
+    secret: 'asecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge: 1000*60*60*24*7
+    }
+}
+app.use(session(sessionConfig));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+     res.locals.currentUser = req.user;
+     next();
+ })
+
+
+app.get('/login', (req, res) => {
     res.send("working");
 })
 
